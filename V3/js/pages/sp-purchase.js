aa9edd6d9f8b5f56f0a30499f86dc9fa41c84839
liangguoqi-0217 <liangguoqi-0217@users.noreply.github.com>
2026-08-11 08:31:07 +0800

    闅愯棌閲囪喘鐢宠鍒楄〃鐨勭鐩垎閰嶇被鍒垪

--- V3/js/pages/sp-purchase.js
+++ V3/js/pages/sp-purchase.js
@@ -144,10 +144,10 @@ const SpPurchase = {
           </div>
         </div>
         <div class="table-wrapper" style="flex:1;">
-          <table class="data-table" style="min-width:1850px;">
+          <table class="data-table" style="min-width:1700px;">
             <thead><tr>
               <th>工厂</th><th>采购申请号</th><th style="width:55px;text-align:center;">行项目</th>
-              <th>物料</th><th>短文本</th><th>申请人</th><th>科目分配类别</th>
+              <th>物料</th><th>短文本</th><th>申请人</th>
               <th>交货日期</th><th style="text-align:right;">数量</th><th style="width:38px;">单位</th>
               <th>采购订单</th><th style="width:65px;">PO行项目</th><th style="text-align:right;">订货数量</th>
               <th style="width:72px;text-align:center;">已结算</th><th style="text-align:right;">评估价格</th>
@@ -217,7 +217,6 @@ const SpPurchase = {
         <td><strong>${esc(row.matCode)}</strong></td>
         <td style="max-width:220px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;" title="${esc(row.shortText)}">${esc(row.shortText)}</td>
         <td>${esc(row.applicant)}</td>
-        <td>${esc(row.acctAssCategory)}</td>
         <td style="white-space:nowrap;">${esc(row.deliveryDate)}</td>
         <td style="text-align:right;">${Number(row.reqQty).toLocaleString()}</td>
         <td style="text-align:center;">${esc(row.unit)}</td>
